0427a7d2391ac8c13211880616d7e432bf42da7e
Raymond Sapida <ray.sapida@gmail.com>
2015-06-04 02:25:13 -0700

    Add async call to maps api

--- js/app.js
+++ js/app.js
@@ -7,4 +7,13 @@ function initialize() {
   google.maps.Map(document.getElementById('map-canvas'),
                   mapOptions);
 }
-google.maps.event.addDomListener(window, 'load', initialize);
+
+function loadScript() {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp' +
+  '&signed_in=true&callback=initialize';
+  document.body.appendChild(script);
+}
+
+window.onload = loadScript;
