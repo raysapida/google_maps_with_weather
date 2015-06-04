87470f516a35ae0c30d6f5a7e8854f3297cf7df2
Raymond Sapida <ray.sapida@gmail.com>
2015-06-04 03:37:08 -0700

    Add an input form for address and use geocode lib to find the lat/long
    https://developers.google.com/maps/documentation/javascript/examples/geocoding-simple

--- js/app.js
+++ js/app.js
@@ -1,11 +1,27 @@
+var geocoder;
+var map;
 function initialize() {
+  geocoder = new google.maps.Geocoder()
   var mapOptions = {
     center: { lat: 37.7833, lng: -122.4167},
     zoom: 8
   };
-  var map = new
-  google.maps.Map(document.getElementById('map-canvas'),
-                  mapOptions);
+ map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
+}
+
+function codeAddress() {
+  var address = document.getElementById('address').value;
+  geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      map.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: map,
+        position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
 }
 
 function loadScript() {
